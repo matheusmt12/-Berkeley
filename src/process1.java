42eade9662453049145443a8c81fff47185ca1d8
matheusmt12 <M@theusmt12>
2023-03-15 10:04:56 -0300

    att

--- src/process1.java
+++ src/process1.java
@@ -1,35 +1,42 @@
 import java.net.DatagramPacket;
+import java.time.*;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 
+public class process1 extends Thread {
 
-public class process1 extends Thread{
-	
-	public static void main(String[] args) {
-		
-	
+	private int identity;
+	private int time;
+	private int port;
+
+	process1(int identity, int time,int port) {
+		this.identity = identity;
+		this.time = time;
+		this.port = port;
+	}
+
+	public void run() {
 		try {
 			byte[] b = "Testando Broadcast1".getBytes();
 
 			InetAddress addr = InetAddress.getByName("255.255.255.255");
-			DatagramPacket pkg = new DatagramPacket(b, b.length, addr,6001);
-			DatagramSocket ds = new DatagramSocket(6002);
-			
+			DatagramPacket pkg = new DatagramPacket(b, b.length, addr, 6001);
+			DatagramSocket ds = new DatagramSocket();
+
 			ds.send(pkg);
-			
+
 			byte[] r = new byte[1024];
 			pkg = new DatagramPacket(r, r.length);
 			ds.receive(pkg);
 			String data = new String(pkg.getData(), 0, pkg.getLength());
 			System.out.println("Dados recebidos: " + data);
 			System.out.println("Teerminou");
-			}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println("Nao foi possivel enviar a mensagem");
 		}
-	
+
 	}
 
 }
--- src/process2.java
+++ src/process2.java
@@ -9,20 +9,10 @@ public class process2 extends Thread{
 	
 	public static void main(String[] args) {
 		
-	
-		try {
-			byte[] b = "Testando Broadcast2".getBytes();
-			//Definindo o endere�o de envio do pacote neste caso o endere�o de broadcast
-			InetAddress addr = InetAddress.getByName("255.255.255.255");
-			DatagramPacket pkg = new DatagramPacket(b, b.length, addr,6001);
-			DatagramSocket ds = new DatagramSocket();
-			
-			ds.send(pkg);//enviando pacote broadcast
-			
-			}
-		catch (Exception e) {
-			System.out.println("Nao foi possivel enviar a mensagem");
-		}
+		process1 t1 = new process1(1,5,7000);
+		t1.start();
+		
+
 	
 	}
 
